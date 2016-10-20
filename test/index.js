6f8e17b6af3f2664ae16f5e54ba5ad3bb6e05139
Daniel <danleavitt0@gmail.com>
2016-10-20 12:15:22 -0700

    plain markdown + standard html tags + inline components

--- test/index.js
+++ test/index.js
@@ -7,7 +7,8 @@ require('babel-register')
 const test = require('tape')
 const markedx = require('..')
 const hyperx = require('hyperx')
-const hx = hyperx(markedx.h)
+var element = require('vdux/element').default
+const hx = hyperx(element)
 
 
 
@@ -16,32 +17,30 @@ const hx = hyperx(markedx.h)
  */
 
 test('should convert plain markdown', (t) => {
-  t.equal(markedx('## Hello World'), '<div><h2 id="hello-world">Hello World</h2>\n</div>')
+  t.equal(markedx('## Hello World'), '<div><h2 id="hello-world">Hello World</h2></div>')
   t.end()
 })
 
-test.only('shouldnt break markdown with standard html tags', (t) => {
+test('shouldnt break markdown with standard html tags', (t) => {
   t.equal(markedx(
 `## Hello World
 I am the foo man.
 <a href="foo"/>
 <h2>Goodbye World</h2>`
-  ), `<div><h2 id="hello-world">Hello World</h2>
-<p>I am the foo man.\n<a href="foo"/></p>
-<h2>Goodbye World</h2></div>`)
+  ), `<div><h2 id="hello-world">Hello World</h2><p>I am the foo man.</p><a href="foo"></a><h2>Goodbye World</h2></div>`)
   t.end()
 })
 
 test('should work with inline component', (t) => {
   markedx.component('header', function ({props, children}) {
-    return hx`<div>i</div>
-    <h2>${children[0]}</h2>`
+    return hx`<div>i</div>`
   })
-
-  console.log('markedx', markedx(
-    `<header>Hello</header>
-  ### hello`
-  ))
+  t.equal(
+    markedx(
+      `<header>Hello</header>
+        ### hello`
+    ), `<div><div>i</div><h3 id="hello">hello</h3></div>`)
+  t.end()
 })
 
 function icon () {
