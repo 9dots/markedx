ea5a005ab5db3297a71633d9212718d204612330
Daniel <danleavitt0@gmail.com>
2016-10-31 16:13:31 -0700

    add lexer

---
+++ test/sample.js
@@ -0,0 +1,80 @@
+module.exports = `In this lesson students explore and predict how they can use loops to more efficiently write code. They will begin to transfer their coding concepts from a paper world to the unplugged platform and in the end will have code they are ready to test online in the next lesson.
+
+[Maze handout][maze-handout]
+
+
+- I can use the debugging process to debug my code.
+- I can replace a repeating sequence with a loop to increase code efficiency and readability.
+
+1. **Do Now (10 min)**
+1. **Small Group Lesson (17 min)**
+  1. **Engage:** Sequence Through a Maze (5 min) 
+	2. **Explore & Explain:** Introducing Loops (7 min) 
+	2. **Elaborate:** Examples in the Real World (5 min) 
+	2. **Extension:** Coding the Enemy (10 min)
+2. **Independent Coding Practice:**
+  1. **https://studio.code.org/s/course2/stage/3/puzzle/1 **
+
+###### Teacher Materials:
+- [ ] Projector
+- [ ] Unplugged Foam Maze 
+- [ ] Whiteboard 
+- [ ] Teacher Scratch Blocks magnetic
+
+
+###### Student Materials: 
+- [ ] [Maze Handout][maze-handout] (class set) - Editable version here
+- [ ] Scratch block strips (1 set per group)
+- [ ] Felt (1 per group)
+- [ ] Role Cards (1 set per group)
+- [ ] Dry erase markers (class set)
+- [ ] Rovers (class set)
+
+<div>
+- **Loop:** A sequence of instructions that is continually repeated until a certain condition is reached.
+- **For-Loop:** A type of loop that specifies the number of times to repeat the nested sequence of instructions.
+</div>
+
+### Do Now (10 min)
+
+- [ ] Explain the new format of our lessons: 
+	- The class will be in 2 groups assigned by the teacher. 
+	- While one group completes a coding lesson with the teacher, the other group practices a coding concept independently.
+	- Halfway through class the groups will switch so that everyone completes both sets of activities.
+- [ ] Demonstrate the independent work
+	- Show students code.org using the tinyurl for the assigned lesson.
+	- Review expectations for using the computers independently. It is important students work silently and troubleshoot independently so that the group in the front of the room can hear the lesson.
+- [ ] Move students into groups
+
+### Small Group Lesson (17 min)
+
+#### Engage: Sequence Through a Maze (5 min)
+- [ ] **Practice Sequence Debugging:** Instruct students to move their rover around their paper maze to **identify where in the given sequence there is a bug**. Students can use a dry erase marker to tick off each line of code as it is completed. 
+- [ ] **Students Propose Solutions:**
+
+
+###### key points
+- [ ] **Perform Solution:** Assign students to group roles to act out the code. 
+
+#### Introducing Loops (7 min)
+- [ ] **Assign Group Roles:** Students are working in groups of 4 for this lesson and should pull group roles. Review each role’s responsibilities. 
+
+- [ ] **Discover Loops:** Working in groups
+	- **Plan:** Students use dry erase marker to draw the path the rover will take around their maze.
+	- **Code:** Students put together a sequence of code on their felt to circumnavigate the grid.  Do not give them enough move and turn blocks. Each group will only have 2 of each type of block. 
+	- **Validate:** Run your code from the beginning to test it.
+
+- [ ] **Add Loop Blocks:** Wait until students identify that there are not enough blocks before introducing loops.  Without introducing them verbally, place loop strips on each group’s table:
+
+- [ ] **Demo Code:** Call on one group to demo their code by bringing their felt up to the board and acting it out on the unplugged maze.
+
+- [ ] **Predict Vocabulary Definitions:** Each group’s driver holds up each loop as you introduce it.
+
+###### key points
+
+#### Loops in the Real World (5 min)
+- [ ] **Teach the Loop Gesture:** Have students move their hand in the loop gesture to remember to read each nested line of code and then return to the start of the loop. 
+- [ ] **Examples of loops in real life:** Have students identify the type of loop you would use for each of the examples below.
+- [ ] **Think Pair Share:** What other examples of loops can you come up with?
+
+[maze-handout]: https://drive.google.com/open?id=0B2wBzr9vcXjPSUV2ZlZtc01Ea2c`
